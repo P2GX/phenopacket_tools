3a1a85117386eb3f657df7f4305593d818782172
Peter Robinson <peter.robinson@jax.org>
2025-05-02 06:56:54 +0200

    more constants

---
+++ src/constants/laterality.rs
@@ -0,0 +1,36 @@
+
+/// Constants for laterality of findings.
+
+
+
+
+pub fn right() -> OntologyClass {
+    OntologyClass {
+        id: "HP:0012834".to_string(),
+        label: "Right".to_string()
+    }
+}
+
+pub fn left() -> OntologyClass {
+    OntologyClass {
+        id: "HP:0012835".to_string(),
+        label: "Left".to_string()
+    }
+}
+
+pub fn unilateral() -> OntologyClass {
+    OntologyClass {
+        id: "HP:0012833".to_string(),
+        label: "Unilateral".to_string()
+    }
+}
+
+pub fn bilateral() -> OntologyClass {
+    OntologyClass {
+        id: "HP:0012832".to_string(),
+        label: "Bilateral".to_string()
+    }
+}
+
+
+
